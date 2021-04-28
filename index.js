1b613015809b08af51462280b3ff0f16aa17ec95
Muhammad Zahid <muhammadali775@gmail.com>
2021-04-27 23:54:51 -0400

    commit tree 66691dc68654cab2bf42092ee03c7bb7e4a6fc1a

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function saturdayFun(thing = "roller-skate") {
+
+ return `This Saturday, I want to ${thing}!`
+}
+
+function mondayWork(activity = "go to the office") {
+
+    return `This Monday, I will ${activity}.`
+
+}
+
+function wrapAdjective(flair = "*"){
+
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!` 
+    
+     }
+}
+
+
+
+
+// if (flair === "*") {
+//            return  () =>  "You are *a hard worker*!"
+// } else if (flair === "||") {
+//            return  () =>  "You are ||a dedicated programmer||!"
+// }
